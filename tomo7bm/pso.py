Bug fixes for averaging filter on the fly. Made testing a config parameter.

tomo7bm/pso.py
@@ -17,8 +17,10 @@ req_end = None
 actual_end = None
 delta_encoder_counts = None
 delta_egu = None
-num_points = None 
+num_proj = None 
+num_images_per_proj = None
 PSO_positions = None
+proj_positions = None
 overall_sense = None
 motor_start = None
 speed = None
@@ -121,6 +123,7 @@ def compute_positions():
     Moreover, we base these on the number of images, not the delta between.
     '''
     global actual_end, delta_egu, delta_encoder_counts, motor_start, motor_end, PSO_positions
+    global proj_positions
     _compute_senses()
     #Get the distance needed for acceleration = 1/2 a t^2 = 1/2 * v * t
     motor_accl_time = driver.motor.acceleration    #Acceleration time in s
@@ -128,7 +131,7 @@ def compute_positions():
 
     #Compute the actual delta to keep things at an integral number of encoder counts
     raw_delta_encoder_counts = (abs(req_end - req_start) 
-                                    / (num_points - 1) * driver.encoder_multiply)
+                                    / ((num_proj - 1) * num_images_per_proj) * driver.encoder_multiply)
     delta_encoder_counts = round(raw_delta_encoder_counts)
     if abs(raw_delta_encoder_counts - delta_encoder_counts) > 1e-4:
         log.warning('  *** *** *** Requested scan would have used a non-integer number of encoder pulses.')
@@ -143,8 +146,10 @@ def compute_positions():
     motor_end = req_end + taxi_dist * user_direction
     
     #Where will the last point actually be?
-    actual_end = req_start + (num_points - 1) * delta_egu * user_direction
-    PSO_positions = np.linspace(req_start, actual_end, num_points)
+    actual_end = req_start + (num_proj * num_images_per_proj- 1) * delta_egu * user_direction
+    end_proj = req_start + (num_proj - 1) * delta_egu * user_direction * num_images_per_proj
+    PSO_positions = np.linspace(req_start, actual_end, num_proj * num_images_per_proj)
+    proj_positions = np.linspace(req_start, end_proj, num_proj) 
     log_info()
 
     
@@ -167,18 +172,23 @@ def log_info():
     log.warning('  *** *** Positions for fly scan.')
     log.info('  *** *** *** Motor start = {0:f}'.format(req_start))
     log.info('  *** *** *** Motor end = {0:f}'.format(actual_end))
-    log.info('  *** *** *** # Points = {0:4d}'.format(num_points))
+    log.info('  *** *** *** # Points = {0:4d}'.format(num_proj))
     log.info('  *** *** *** Degrees per image = {0:f}'.format(delta_egu))
+    log.info('  *** *** *** Degrees per projection = {0:f}'.format(delta_egu / num_images_per_proj))
     log.info('  *** *** *** Encoder counts per image = {0:d}'.format(delta_encoder_counts))
 
 
 def pso_init(params):
     '''Initialize calculations.
     '''
-    global req_start, req_end, num_points, speed
+    global req_start, req_end, num_proj, num_images_per_proj, speed
     req_start = params.sample_rotation_start
     req_end = params.sample_rotation_end
-    num_points = params.num_projections
+    num_proj = params.num_projections
+    if params.recursive_filter:
+        num_images_per_proj = params.recursive_filter_n_images
+    else:
+        num_images_per_proj = 1
     speed = params.slew_speed
     driver.default_speed = params.retrace_speed
     compute_positions()
@@ -190,7 +200,7 @@ def fly(global_PVs, params):
     log.warning('  *** Fly Scan Time Estimate: %4.2f minutes' % (flyscan_time_estimate/60.))
     #Trigger fly motion to start.  Don't wait for it, since it takes time.
     start_time = time.time()
-    driver.motor.move(actual_end, wait=False)
+    driver.motor.move(motor_end, wait=False)
     time.sleep(1)
     old_image_counter = 0
     expected_framerate = driver.motor.slew_speed / delta_egu
@@ -199,7 +209,7 @@ def fly(global_PVs, params):
         time.sleep(1)
         if not driver.motor.moving:
             log.info('  *** *** Sample rotation stopped moving.')
-            if abs(driver.motor.drive - actual_end) > 1e-2:
+            if abs(driver.motor.drive - motor_end) > 1e-2:
                 log.error('  *** *** Sample rotation ended but not at right position!')
                 raise ValueError
             else:

tomo7bm/scan.py
@@ -63,10 +63,6 @@ def fly_scan(params):
             #dm.scp(global_PVs, params)
 
         log.info('  *** Total loop scan time: %s minutes' % str((time.time() - tic)/60.))
-
-        log.info('  *** Moving rotary stage to start position')
-        global_PVs["Motor_SampleRot"].put(0, wait=True, timeout=600.0)
-        log.info('  *** Moving rotary stage to start position: Done!')
         global_PVs['Cam1_ImageMode'].put('Continuous')
         log.info('  *** Done!')
     except  KeyError:
@@ -294,6 +290,9 @@ def tomo_fly_scan(global_PVs, params):
     flir.checkclose_hdf(global_PVs, params)
     flir.add_theta(global_PVs, params)
 
+    # If requested, move rotation stage back to zero
+    pso.cleanup_PSO()
+    pso.driver.motor.move(pso.req_start, wait=False)
     # update config file
     config.update_config(params)
 
@@ -310,10 +309,8 @@ def set_slew_speed(global_PVs, params):
     '''
     log.info('  *** Calculate slew speed and blur')
     scan_range = abs(params.sample_rotation_start - params.sample_rotation_end)
-    images_per_proj = 1
-    if params.recursive_filter and int(params.recursive_filter_n_images) > 1:
-        images_per_proj = int(params.recursive_filter_n_images)
-    delta_angle = scan_range / params.num_projections / images_per_proj
+    set_image_factor(global_PVs, params)
+    delta_angle = scan_range / (params.num_projections - 1) / params.recursive_filter_n_images 
     data_max_framerate = flir.calc_max_framerate(global_PVs, params)
     acq_max_framerate = 1.0 / (params.exposure_time + params.ccd_readout)
     im_half_width = global_PVs['Cam1_MaxSizeX_RBV'].get() / 2.0
@@ -332,6 +329,8 @@ def set_slew_speed(global_PVs, params):
         return finish_set_slew_speed(global_PVs, params)
     elif params.auto_slew_speed == 'acqusition':
         log.info('  *** *** Calc slew speed from data throughput and exposure limits.')
+    elif params.recursive_filter_n_images > 1:
+        log.warning('  *** *** Blur calculation makes less sense with averaging in each projection.')
     else:
         log.info('  *** *** Calc slew speed based on blur and acquisition parameters.')
         max_blur_angle = np.degrees(np.arcsin(params.permitted_blur / im_half_width))
@@ -346,12 +345,9 @@ def set_slew_speed(global_PVs, params):
         log.info('  *** *** Limited by data throughput to {:6.3f} Hz'.format(data_max_framerate))
     else:
         log.info('  *** *** Limited by acquisition time to {:6.3f} Hz'.format(acq_max_framerate))
-    return finish_set_slew_speed(global_PVs, params)
-
-
-def finish_set_slew_speed(global_PVs,params):
-    im_half_width = global_PVs['Cam1_MaxSizeX_RBV'].get() / 2.0
     blur = np.sin(np.radians(params.slew_speed * params.exposure_time)) * im_half_width 
+    if params.recursive_filter_n_images > 1:
+        blur = np.sin(np.radians(delta_angle)) * im_half_width    
     if blur > params.permitted_blur:
         log.warning('  *** *** Motion blur on image edge = {:6.2f} px'.format(blur))
     else:
@@ -374,7 +370,7 @@ def move_sample_out(global_PVs, params):
         #Check the limits
         out_x = params.sample_out_x
         out_y = params.sample_out_y
-        log.info('Moving to (x,y) = ({0:6.4f}, {1:6.4f})'.format(out_x, out_y))
+        log.info('      *** Moving to (x,y) = ({0:6.4f}, {1:6.4f})'.format(out_x, out_y))
         if not (global_PVs['Motor_SampleX'].within_limits(out_x)
                and global_PVs['Motor_SampleY'].within_limits(out_y)):
             log.error('        *** *** Sample out position past motor limits.')
@@ -405,6 +401,8 @@ def move_sample_in(global_PVs, params):
         except AttributeError:
             params.original_x = global_PVs['Motor_SampleX'].drive 
             params.original_y = global_PVs['Motor_SampleY'].drive 
+        log.info('      *** Moving to (x,y) = ({0:6.4f}, {1:6.4f})'.format(
+                    params.original_x, params.original_y))
         #Check the limits
         if not (global_PVs['Motor_SampleX'].within_limits(params.original_x)
                and global_PVs['Motor_SampleY'].within_limits(params.original_y)):